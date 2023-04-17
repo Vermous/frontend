d6e6b1bac4afb370ce72536adf9967050664705c
Vermouslulu <59926360+Vermous@users.noreply.github.com>
2023-04-17 14:44:54 -0400

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+//import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+//import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
+
+const Homes = () => {
+ // const { currentColor, currentMode } = useStateContext();
+
+  return (
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50 "
+              src={product9}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg">The new Trashcans are coming!</p>
+              <p className="text-gray-400 ">By A+ Team</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+            </div>
+          </div>
+  );
+};
+
+export default Homes;
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Home } from './Home';
+export { default as Employees } from './Employees';
+export { default as Users } from './Users';
+export { default as Calendar } from './Calendar';
+export { default as Bar } from './Charts/Bar';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Map } from './Map';
